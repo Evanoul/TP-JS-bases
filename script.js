f03fee0726f67a587129110db0ab34e3f25ccaa3
Evanoul <evancantatore4@gmail.com>
2025-10-03 14:47:50 +0000

    feat: Exercices sur les fondamentaux JS terminés

---
+++ script.js
@@ -0,0 +1,28 @@
+const nom = "Evan";
+console.log(nom); // Affiche "Evan"
+
+let age = 16;
+console.log(age); // Affiche 16
+
+
+if (age < 13) {
+    console.log("Accès interdit aux moins de 13 ans."); // affiche "Accès interdit aux moins de 13 ans." si age < 13
+
+} else if (age < 18) {
+    console.log("Vous pouvez accéder à la section ado."); // affiche "Vous pouvez accéder à la section ado." si age < 18
+
+} else {
+    console.log("Bienvenue dans l'espace adulte."); // affiche "Bienvenue dans l'espace adulte." dans tout les autres cas
+
+
+} for (let i = 0; i < 11; i++) { // met la variable i à 0, et ajoute 1 à n tant que i < 11   (7 x n =blabla)
+    console.log("7x" + i + "= " + (i*7));
+
+}
+
+const parler = (nom) => {
+    const saluer = ("Bonjour " + nom + "! " + "Comment vas tu?");
+    return saluer;
+}
+const saluer = parler(nom);
+console.log(saluer)
